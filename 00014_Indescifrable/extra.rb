0a1009706895ef1d0b923ab9fbfb2df55abab049
Gustavo Trucco <Gustavo Trucco>
2019-02-06 23:48:17 +0000

    Mumuki Export on 2019-02-06 23:48:17 +0000

--- 00014_Indescifrable/extra.rb
+++
@@ -1,8 +0,0 @@
-def hash(mensaje)
-	tamanio_mensaje = mensaje.length
-	mensaje
-		.chars
-		.each_with_index
-		.map { |letra, posicion_letra| letra.ord * (31 ** (tamanio_mensaje - (posicion_letra + 1))) }
-		.sum
-end
